fba282ed4f3d05407c6495ef72ca4f1153683610
Ajay Gitala <i-ajay.gitala@devrev.ai>
2026-05-21 01:02:20 +0530

    chore: Add config library viper

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import "github.com/spf13/viper"
+import "strings"
+
+type Config struct {
+	Server   ServerConfig   `mapstructure:"server"`
+	Database DatabaseConfig `mapstructure:"database"`
+	Redis    RedisConfig    `mapstructure:"redis"`
+	Cors     CORSConfig     `mapstructure:"cors"`
+}
+
+type ServerConfig struct {
+	Port    int    `mapstructure:"port"`
+	BaseURL string `mapstructure:"base_url"`
+}
+
+type DatabaseConfig struct {
+	URL string `mapstructure:"url"`
+}
+
+type RedisConfig struct {
+	URL string `mapstructure:"url"`
+	TTL string `mapstructure:"ttl"`
+}
+
+type CORSConfig struct {
+	AllowedOrigins []string `mapstructure:"allowed_origins"`
+	AllowedMethods []string `mapstructure:"allowed_methods"`
+	AllowedHeaders []string `mapstructure:"allowed_headers"`
+}
+
+func Load() (Config, error) {
+	v := viper.New()
+
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	v.AddConfigPath(".")
+
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+
+	if err := v.ReadInConfig(); err != nil {
+		return Config{}, err
+	}
+
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err != nil {
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
